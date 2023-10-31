1ee789cdac768dcf3cea4784a1870205708fc594
Isak <16906089+hanssonduck@users.noreply.github.com>
2023-10-31 20:02:34 +0100

    Switch to bun

--- src/lib/server/graphql/handler.ts
+++ src/lib/server/graphql/handler.ts
@@ -2,24 +2,4 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { createYoga } from 'graphql-yoga';
 import { schema } from './schema';
 
-export const handler = createYoga({
-	fetchAPI: {
-		fetch,
-		Request,
-		Response,
-		Headers,
-		FormData,
-		ReadableStream,
-		WritableStream,
-		TransformStream,
-		Blob,
-		crypto,
-		btoa,
-		TextEncoder,
-		TextDecoder,
-		URLPattern,
-		URL,
-		URLSearchParams
-	},
-	schema
-}) satisfies RequestHandler;
+export const handler = createYoga({ schema }) satisfies RequestHandler;
